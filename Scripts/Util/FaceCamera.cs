7aea426ac9469f48a8962745a871c05d0d1f9224
Erkberg <erisch01@web.de>
2019-11-17 13:45:07 +0100

    face camera script

---
+++ Scripts/Util/FaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public class FaceCamera : MonoBehaviour
+    {
+        public Transform cam;
+
+        private void Start()
+        {
+            if (!cam)
+                cam = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            transform.LookAt(cam);
+        }
+    }
+}
