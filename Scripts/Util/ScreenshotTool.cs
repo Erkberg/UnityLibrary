9c739905e6fb4a008952631dd3e95a5f5a9702a9
Erkberg <erisch01@web.de>
2020-01-22 17:50:58 +0100

    screenshotTool

---
+++ Scripts/Util/ScreenshotTool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public class ScreenshotTool : MonoBehaviour
+    {
+        public KeyCode screenshotKey = KeyCode.F8;
+        public string filename = "Screeny";
+        public int superSize = 1;
+        public bool addTimeString = true;
+
+        private const string fileEnding = ".png";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(screenshotKey))
+            {
+                string screenyName = filename;
+
+                if (addTimeString)
+                    screenyName += "_" + Regex.Replace(System.DateTime.Now.TimeOfDay.ToString().Split('.')[0], ":", "-");
+
+                screenyName += fileEnding;
+
+                ScreenCapture.CaptureScreenshot(screenyName, 1);
+                Debug.Log("Captured screenshot of name " + screenyName);
+            }
+        }
+    }
+}
