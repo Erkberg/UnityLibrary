e9cf1cbeffc88c28365eb625adeebc56584c152a
Erkberg <erisch01@web.de>
2020-03-29 13:33:55 +0200

    ScreenUtils (relative mouse position inside screen)

---
+++ Scripts/Util/ScreenUtils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+	public static class ScreenUtils
+	{
+        public static bool IsMouseInsideScreen()
+        {
+            return Input.mousePosition.x >= 0f && Input.mousePosition.x <= Screen.width &&
+                Input.mousePosition.y >= 0f && Input.mousePosition.y <= Screen.height;
+        }
+
+        public static Vector2 GetMousePositionScreenPercentage()
+        {
+            return new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+        }
+
+        public static Vector2 GetMouseDistancePercentageFromCenter()
+        {
+            return (GetMousePositionScreenPercentage() - Vector2.one / 2) * 2;
+        }
+    }
+}
