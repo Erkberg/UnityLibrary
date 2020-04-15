2affb77836309352f8ea2b304b8ed31c5d003ab3
Erkberg <erisch01@web.de>
2020-04-15 21:06:15 +0200

    fix flyingNumbers and have it commented by default (can uncomment if DOTween is present)

--- Scripts/Text/FlyingNumbers.cs
+++ Scripts/Text/FlyingNumbers.cs
@@ -1,163 +1,182 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// This class currently only works when DOTween is present
+// Add the DOTween plugin to your project and comment this conditional to use this class
+#if DOTWEEN
+using DG.Tweening;
 
-//public class FlyingNumbers : MonoBehaviour
-//{
-//    [Header("Position")]
-//    public Transform targetTransform;
-//    public bool moveWithTargetTransform = true;
-//    public Vector3 initialOffset = Vector3.zero;
-//    public Vector3 finalOffset = new Vector3(0f, 1f, 0f);
+public class FlyingNumbers : MonoBehaviour
+{
+    [Header("Position")]
+    public Transform targetTransform;
+    public bool moveWithTargetTransform = true;
+    public Vector3 initialOffset = Vector3.zero;
+    public Vector3 finalOffset = new Vector3(0f, 1f, 0f);
 
-//    [Header("Movement")]
-//    public float movementDuration = 1f;
-//    public float delayBeforeDisappear = 0f;
-//    public Ease movementEase = Ease.OutCubic;
+    [Header("Movement")]
+    public float movementDuration = 1f;
+    public float delayBeforeDisappear = 0f;
+    public Ease movementEase = Ease.OutCubic;
 
-//    [Header("Fading")]
-//    public bool fade = true;
-//    public float fadeInDuration = 0.5f;
-//    public Ease fadeInEase = Ease.InQuart;
-//    public float fadeOutDuration = 0.5f;
-//    public Ease fadeOutEase = Ease.OutQuart;
+    [Header("Fading")]
+    public bool fade = true;
+    public float fadeInDuration = 0.5f;
+    public Ease fadeInEase = Ease.InQuart;
+    public float fadeOutDuration = 0.5f;
+    public Ease fadeOutEase = Ease.OutQuart;
 
-//    [Header("Text")]
-//    public bool useOnlyOneText = false;
-//    public Text protoText;
-//    private List<Text> texts = new List<Text>();
-//    private Text currentText;
+    [Header("Text")]
+    public bool useOnlyOneText = false;
+    public Text protoText;
+    private List<Text> texts = new List<Text>();
+    private Text currentText;
 
-//    public List<Color> colors;
-//    private Color currentColor;
+    public List<Color> colors;
+    private Color currentColor;
 
-//    public bool faceCamera = true;
+    public bool faceCamera = true;
 
-//    private float currentScale = 1f;
-//    private int initalTextSize;
+    private float currentScale = 1f;
+    private int initalTextSize;
 
-//    private void Awake()
-//    {
-//        if (!protoText)
-//            protoText = GetComponentInChildren<Text>();
-//    }
+    private void Awake()
+    {
+        if (!protoText)
+            protoText = GetComponentInChildren<Text>();
+    }
 
-//    // Use this for initialization
-//    void Start ()
-//    {
-//        transform.position = targetTransform.position + initialOffset;
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = targetTransform.position;
 
-//        texts.Add(protoText);
-//        initalTextSize = protoText.fontSize;
+        texts.Add(protoText);
+        initalTextSize = protoText.fontSize;
 
-//        if(colors.Count > 0)
-//        {
-//            currentColor = colors[0];
-//        }
-//        else
-//        {
-//            currentColor = Color.white;
-//        }        
-//	}
-	
-//    public void StartFlying(string text)
-//    {
-//        FindAvailableText();
+        if (colors.Count > 0)
+        {
+            currentColor = colors[0];
+        }
+        else
+        {
+            currentColor = Color.white;
+        }
+    }
 
-//        currentText.transform.localPosition = Vector3.zero;
-//        currentText.text = text;        
-//        currentText.fontSize = (int)(initalTextSize * currentScale);
+    private void Update()
+    {
+        if(moveWithTargetTransform && targetTransform)
+            transform.position = targetTransform.position;
+    }
 
-//        StartCoroutine(FlyingSequence(currentText));
-//    }
+    public void StartFlying(string text)
+    {
+        FindAvailableText();
 
-//    private IEnumerator FlyingSequence(Text text)
-//    {
-//        yield return new WaitForEndOfFrame();
+        if(moveWithTargetTransform)
+        {
+            currentText.transform.localPosition = initialOffset;
+        }
+        else
+        {
+            currentText.transform.position = targetTransform.position + initialOffset;
+        }
+        
+        currentText.text = text;
+        currentText.fontSize = (int)(initalTextSize * currentScale);
 
-//        if (fade)
-//        {
-//            StartCoroutine(FadeSequence(text));
-//        }
-//        else
-//        {
-//            text.color = currentColor;
-//        }
+        StartCoroutine(FlyingSequence(currentText));
+    }
 
-//        if (moveWithTargetTransform)
-//        {
-//            text.transform.DOLocalMove(finalOffset, movementDuration).SetEase(movementEase);
-//        }
-//        else
-//        {
-//            text.transform.DOMove(finalOffset, movementDuration).SetEase(movementEase);
-//        }
+    private IEnumerator FlyingSequence(Text text)
+    {
+        yield return new WaitForEndOfFrame();
 
-//        yield return new WaitForSeconds(movementDuration);
-//        yield return new WaitForSeconds(delayBeforeDisappear);
+        if (fade)
+        {
+            StartCoroutine(FadeSequence(text));
+        }
+        else
+        {
+            text.color = currentColor;
+        }
 
-//        text.text = "";
-//    }
+        if (moveWithTargetTransform)
+        {
+            text.transform.DOLocalMove(finalOffset, movementDuration).SetEase(movementEase);
+        }
+        else
+        {
+            text.transform.DOMove(targetTransform.position + finalOffset, movementDuration).SetEase(movementEase);
+        }        
 
-//    private IEnumerator FadeSequence(Text text)
-//    {
-//        Color transparent = new Color(currentColor.r, currentColor.g, currentColor.b, 0f);
-//        text.color = transparent;
-//        text.DOColor(currentColor, fadeInDuration).SetEase(fadeInEase);
+        yield return new WaitForSeconds(movementDuration);
+        yield return new WaitForSeconds(delayBeforeDisappear);
 
-//        float fadeDelay = movementDuration + delayBeforeDisappear - fadeInDuration - fadeOutDuration;
-//        if (fadeDelay < 0f)
-//            fadeDelay = 0f;
+        text.text = "";
+    }
 
-//        yield return new WaitForSeconds(fadeInDuration);
+    private IEnumerator FadeSequence(Text text)
+    {
+        Color transparent = new Color(currentColor.r, currentColor.g, currentColor.b, 0f);
+        text.color = transparent;
+        text.DOColor(currentColor, fadeInDuration).SetEase(fadeInEase);
 
-//        yield return new WaitForSeconds(fadeDelay);
+        float fadeDelay = movementDuration + delayBeforeDisappear - fadeInDuration - fadeOutDuration;
+        if (fadeDelay < 0f)
+            fadeDelay = 0f;
 
-//        text.DOColor(transparent, fadeOutDuration).SetEase(fadeOutEase);
+        yield return new WaitForSeconds(fadeInDuration);
 
-//        yield return new WaitForSeconds(fadeOutDuration);
-//    }
-    
-//    public void StartFlying(int number)
-//    {
-//        StartFlying(number.ToString());
-//    }
+        yield return new WaitForSeconds(fadeDelay);
 
-//    public void SetColor(int colorId)
-//    {
-//        if(colorId > colors.Count)
-//        {
-//            currentColor = colors[colorId];
-//        }
-//    }
+        text.DOColor(transparent, fadeOutDuration).SetEase(fadeOutEase);
 
-//    public void SetScale(float newScale)
-//    {
-//        currentScale = newScale;
-//    }
+        yield return new WaitForSeconds(fadeOutDuration);
+    }
 
-//    private void FindAvailableText()
-//    {
-//        if(useOnlyOneText)
-//        {
-//            StopAllCoroutines();
-//            protoText.DOKill();
-//            protoText.transform.DOKill();
-//            currentText = protoText;
-//            return;
-//        }
+    public void StartFlying(int number)
+    {
+        StartFlying(number.ToString());
+    }
 
-//        foreach(Text text in texts)
-//        {
-//            if(text.text == "")
-//            {
-//                currentText = text;
-//                return;
-//            }
-//        }
+    public void SetColor(int colorId)
+    {
+        if (colorId > colors.Count)
+        {
+            currentColor = colors[colorId];
+        }
+    }
 
-//        texts.Add(Instantiate(protoText, transform.position, Quaternion.identity, transform));
-//        currentText = texts[texts.Count - 1];
-//    }
-//}
+    public void SetScale(float newScale)
+    {
+        currentScale = newScale;
+    }
+
+    private void FindAvailableText()
+    {
+        if (useOnlyOneText)
+        {
+            StopAllCoroutines();
+            protoText.DOKill();
+            protoText.transform.DOKill();
+            currentText = protoText;
+            return;
+        }
+
+        foreach (Text text in texts)
+        {
+            if (text.text == "")
+            {
+                currentText = text;
+                return;
+            }
+        }
+
+        texts.Add(Instantiate(protoText, transform.position, Quaternion.identity, transform));
+        currentText = texts[texts.Count - 1];
+    }
+}
+#endif
