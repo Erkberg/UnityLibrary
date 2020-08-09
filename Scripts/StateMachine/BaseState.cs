91af1e1f8ed9c719a25ceb04875288501b888dc7
Erkberg <erisch01@web.de>
2020-08-09 18:54:22 +0200

    basic state machine

---
+++ Scripts/StateMachine/BaseState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public class BaseState
+    {
+        public string name;
+
+        public BaseState()
+        {
+        }
+
+        public BaseState(string name)
+        {
+            this.name = name;
+        }
+        
+        public virtual void Enter() { }
+        
+        public virtual void Update() { }
+        public virtual void Exit() { }
+    }
+}
+
---
+++ Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public class StateMachine
+    {
+        List<BaseState> states = new List<BaseState>();
+        BaseState currentState;
+
+        public void AddState(BaseState state)
+        {
+            states.Add(state);
+        }
+ 
+        public void ChangeState(BaseState newState)
+        {
+            currentState?.Exit();
+            currentState = newState;
+            currentState.Enter();
+        }
+        
+        public void ChangeState(string newStateName)
+        {
+            BaseState newState = GetStateByName(newStateName);
+
+            if (newState != null)
+            {
+                ChangeState(newState);
+            }
+        }
+ 
+        public void Update()
+        {
+            currentState?.Update();
+        }
+        
+        private BaseState GetStateByName(string name)
+        {
+            BaseState matchedState = null;
+
+            foreach (BaseState state in states)
+            {
+                if (state.name == name)
+                {
+                    matchedState = state;
+                    break;
+                }
+            }
+
+            return matchedState;
+        }
+    }
+}
