f29b8a2228824591e206d5f30c33197e8f8ecffb
Erkberg <erisch01@web.de>
2021-03-18 19:07:26 +0100

    generic extension methods to get next and previous enum value

---
+++ Scripts/ExtensionMethods/GenericExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public static class GenericExtensions
+    {
+        public static T Next<T>(this T src) where T : Enum
+        {
+            T[] values = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf(values, src) + 1;
+            return (values.Length == j) ? values[0] : values[j];            
+        }
+        
+        public static T Previous<T>(this T src) where T : Enum
+        {
+            T[] values = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf(values, src) - 1;
+            return (j == -1) ? values[values.Length - 1] : values[j];            
+        }
+    }
+}
