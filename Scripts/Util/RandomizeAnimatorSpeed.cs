7f8c16fa49f04eb7b4eca9f7dc8de44c3d4b2a6f
Erkberg <erisch01@web.de>
2021-05-11 17:51:47 +0200

    add util class RandomizeAnimatorSpeed

---
+++ Scripts/Util/RandomizeAnimatorSpeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimatorSpeed : MonoBehaviour
+{
+    public Animator animator;
+    public float minValue = 0.67f;
+    public float maxValue = 1.33f;
+    
+    void Awake()
+    {
+        if (!animator)
+            animator = GetComponent<Animator>();
+
+        if (animator)
+            animator.speed = Random.Range(minValue, maxValue);
+    }    
+}
