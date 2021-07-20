b4af873282b48b07874aeebdfb99c1cf8e6c8429
Erkberg <erisch01@web.de>
2021-07-20 17:03:11 +0200

    add some particle extensions

--- Scripts/ExtensionMethods/ParticleExtensions.cs
+++ Scripts/ExtensionMethods/ParticleExtensions.cs
@@ -5,12 +5,79 @@ namespace ErksUnityLibrary
 {
     public static class ParticleExtensions
     {
+        public static void SetEmissionEnabled(this ParticleSystem particles, bool enabled)
+        {
+            if (particles)
+            {
+                var em = particles.emission;
+                em.enabled = enabled;
+            }
+            else
+            {
+                Debug.Log("no particles object");
+            }
+        }
+
         public static void SetEmissionOverTime(this ParticleSystem particles, float value)
         {
             if (particles)
             {
                 var em = particles.emission;
                 em.rateOverTime = value;
+            }
+            else
+            {
+                Debug.Log("no particles object");
+            }
+        }
+        
+        public static void AddToEmissionOverTime(this ParticleSystem particles, float valueToAdd)
+        {
+            if (particles)
+            {
+                var em = particles.emission;
+                float rate = em.rateOverTime.constant + valueToAdd;
+                em.rateOverTime = rate;
+            }
+            else
+            {
+                Debug.Log("no particles object");
+            }
+        }
+
+        public static void SetLifetime(this ParticleSystem particles, float value)
+        {
+            if (particles)
+            {
+                var main = particles.main;
+                main.startLifetime = value;
+            }
+            else
+            {
+                Debug.Log("no particles object");
+            }
+        }
+        
+        public static void AddToLifetime(this ParticleSystem particles, float valueToAdd)
+        {
+            if (particles)
+            {
+                var main = particles.main;
+                float lifetime = main.startLifetime.constant + valueToAdd;
+                main.startLifetime = lifetime;
+            }
+            else
+            {
+                Debug.Log("no particles object");
+            }
+        }
+
+        public static void SetStartColor(this ParticleSystem particles, Color color)
+        {
+            if (particles)
+            {
+                var main = particles.main;
+                main.startColor = color;
             }
             else
             {
@@ -32,20 +99,6 @@ namespace ErksUnityLibrary
             }
 
             return position;
-        }
-        
-        public static void AddToEmissionOverTime(this ParticleSystem particles, float valueToAdd)
-        {
-            if (particles)
-            {
-                var em = particles.emission;
-                float rate = em.rateOverTime.constant + valueToAdd;
-                em.rateOverTime = rate;
-            }
-            else
-            {
-                Debug.Log("no particles object");
-            }
         }
     }
 }
