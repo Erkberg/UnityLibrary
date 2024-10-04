fced69ab67975b1a87bce9bab90f4cef3e12a0de
Erkberg <erisch01@web.de>
2024-10-04 10:16:08 +0200

    add health component

---
+++ Scripts/Components/HealthComponent.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErksUnityLibrary
+{
+    public class HealthComponent : MonoBehaviour
+    {
+        public float maxHealth = 1;
+        public float currentHealth = 1;
+
+        public event Action died;
+        public event Action healthChanged;
+        public event Action healthIncreased;
+        public event Action healthDecreased;
+
+        public void Damage(float amount)
+        {
+            if (currentHealth > 0)
+            {
+                healthDecreased?.Invoke();
+                healthChanged?.Invoke();
+            }
+
+            currentHealth -= amount;
+            ClampCurrentHealth();
+
+            if (currentHealth <= 0)
+            {
+                died?.Invoke();
+            }
+        }
+
+        public void Heal(float amount)
+        {
+            if (currentHealth < maxHealth)
+            {
+                healthIncreased?.Invoke();
+                healthChanged?.Invoke();
+            }
+
+            currentHealth += amount;
+            ClampCurrentHealth();
+        }
+
+        public float GetHealthPercentage()
+        {
+            return currentHealth / maxHealth;
+        }
+
+        private void ClampCurrentHealth()
+        {
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        }
+    }
+}
